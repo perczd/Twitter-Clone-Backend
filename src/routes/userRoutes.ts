0581c6b6fbd4ff37d4a705b328b93a0c532b86e7
perczd <136338218+perczd@users.noreply.github.com>
2023-08-02 15:09:50 +0000

    Add files via upload

---
+++ src/routes/userRoutes.ts
@@ -0,0 +1,67 @@
+import { Router } from "express";
+import { PrismaClient } from "@prisma/client";
+
+const router = Router();
+const prisma = new PrismaClient();
+
+router.post('/', async (req, res) => {
+    const { email, name, username} = req.body;
+    try {
+        const result = await prisma.user.create({
+            data: {
+                email,
+                name,
+                username,
+                bio: "Hello I'm new on Twitter",
+            },
+        });
+    
+    res.json(result);
+    } catch (e) {
+        res.status(400).json({ error: 'Username and email should be unique' });
+    }
+});
+
+router.get('/', async (req, res) => {
+    const allUser = await prisma.user.findMany({ 
+        select: { id: true, name: true, image: true, bio: true,},
+    });
+
+    res.json(allUser);
+});
+
+router.get('/:id', async (req, res) => {
+    const {id} = req.params;
+    const user = await prisma.user.findUnique({ 
+        where: {id: Number(id) },
+        include: { tweets: true },
+     });
+    if (!user) {
+        return  res.status(404).json({error: "Tweet not found!"});
+      }
+    res.json(user);
+});
+
+router.put('/:id', async (req, res) => {
+    const {id} = req.params;
+    const {bio, name, image, username} =req.body;
+
+    try {
+        const result = await prisma.user.update({
+            where: { id: Number(id)},
+            data: {bio, name, image, username}
+        });
+        res.json(result);
+    } catch(e) {
+        res.status(400).json({error: `Failed to update the user` });
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    const {id} = req.params;
+    await prisma.user.delete({where: {id: Number(id)}})
+    res.sendStatus(200);
+});
+
+
+export default router;
